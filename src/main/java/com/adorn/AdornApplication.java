a9e8c1f390dcb551795c4654cc6db8c42c1ab173
Harzh-k <harzhk2002@gmail.com>
2024-12-09 13:18:06 +0530

    first commit

---
+++ src/main/java/com/adorn/AdornApplication.java
@@ -0,0 +1,13 @@
+package com.adorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adorn/AdornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
